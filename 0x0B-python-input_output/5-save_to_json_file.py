93759e13d8852fd40a801aa4c29015a16c8683c0
Ukamaka4398 <egwuukamaka4398@gmail.com>
2023-02-12 01:56:29 -0800

    corrected

--- 0x0B-python-input_output/5-save_to_json_file.py
+++ 0x0B-python-input_output/5-save_to_json_file.py
@@ -1,7 +1,6 @@
-!/usr/bin/python3
+#!/usr/bin/python3
 """Defines a JSON file-writing function."""
 import json
-
 
 def save_to_json_file(my_obj, filename):
     """Write an object to a text file using JSON representation."""
